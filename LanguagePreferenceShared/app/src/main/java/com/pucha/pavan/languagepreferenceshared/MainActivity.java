7313c762c944c21f5d5c5272a6d635ec90342b6f
pavanpucha <pavanrajasekhar@gmail.com>
2017-07-31 17:36:17 -0400

    Language Pref Alert Dialogue

--- LanguagePreferenceShared/app/src/main/java/com/pucha/pavan/languagepreferenceshared/MainActivity.java
+++ LanguagePreferenceShared/app/src/main/java/com/pucha/pavan/languagepreferenceshared/MainActivity.java
@@ -1,5 +1,9 @@
 package com.pucha.pavan.languagepreferenceshared;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SharedPreferences newSharedPreferences = this.getSharedPreferences("com.pucha.pavan.languagepreferenceshared", Context.MODE_PRIVATE);
+        String languageSelected = newSharedPreferences.getString("language","");
+        if(languageSelected ==""){
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle("What language ?")
+                    .setMessage("English or spanish")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // Change language to english
+                            newSharedPreferences.edit().putString("language","english");
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            newSharedPreferences.edit().putString("language","spanish");
+                        }
+                    })
+                    .show();
+
+        }
     }
 }
