72198da1eb530a94d1df7172a0f5b5866e7a069e
pavanpucha <pavanrajasekhar@gmail.com>
2017-08-01 18:32:25 -0400

    Data Persistance Established

--- Notes/app/src/main/java/com/pucha/pavan/notes/EditYourNote.java
+++ Notes/app/src/main/java/com/pucha/pavan/notes/EditYourNote.java
@@ -1,6 +1,8 @@
 package com.pucha.pavan.notes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,11 +13,17 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static android.os.Build.VERSION_CODES.M;
 import static com.pucha.pavan.notes.MainActivity.notes;
+import static com.pucha.pavan.notes.MainActivity.set;
 
 public class EditYourNote extends AppCompatActivity implements TextWatcher {
     int noteId;
+   // SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +31,9 @@ public class EditYourNote extends AppCompatActivity implements TextWatcher {
         EditText editText = (EditText) findViewById(R.id.editText);
         //getting content from intent
         Intent i = getIntent();
-        int noteId = i.getIntExtra("noteId",-1);
-        if(noteId != -1){
-           editText.setText(MainActivity.notes.get(noteId));
+        int noteId = i.getIntExtra("noteId", -1);
+        if (noteId != -1) {
+            editText.setText(MainActivity.notes.get(noteId));
 
         }
 
@@ -39,8 +47,16 @@ public class EditYourNote extends AppCompatActivity implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        MainActivity.notes.set(noteId,String.valueOf(charSequence));
+        MainActivity.notes.set(noteId, String.valueOf(charSequence));
         MainActivity.arrayAdapter.notifyDataSetChanged();
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.pucha.pavan.notes", Context.MODE_PRIVATE);
+        if(MainActivity.set == null){
+            MainActivity.set = new HashSet<String>();
+        }else
+            MainActivity.set.clear();
+
+        MainActivity.set.addAll(MainActivity.notes);
+        sharedPreferences.edit().putStringSet("notes", set).apply();
     }
 
     @Override
--- Notes/app/src/main/java/com/pucha/pavan/notes/MainActivity.java
+++ Notes/app/src/main/java/com/pucha/pavan/notes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pucha.pavan.notes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,16 +11,33 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     static ArrayList<String> notes= new ArrayList<>();
     static ArrayAdapter arrayAdapter;
+    static  Set<String> set;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView listView = (ListView) findViewById(R.id.listView);
-        notes.add("Example Note");
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.pucha.pavan.notes", Context.MODE_PRIVATE);
+        set = sharedPreferences.getStringSet("notes",null);
+        if(set!=null){
+            notes.clear();
+            notes.addAll(set);
+        }
+        else
+        {
+          //  notes.clear();
+            notes.add("example Note");
+            set = new HashSet<String>();
+            set.addAll(notes);
+            sharedPreferences.edit().putStringSet("notes",set).apply();
+        }
+               
         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
