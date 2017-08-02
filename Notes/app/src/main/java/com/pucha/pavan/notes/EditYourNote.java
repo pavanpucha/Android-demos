15a6de90a9c69ff83aa08e52ff4473afe54988c6
pavanpucha <pavanrajasekhar@gmail.com>
2017-08-02 18:27:04 -0400

    added delete functionality on long press of listview

--- Notes/app/src/main/java/com/pucha/pavan/notes/EditYourNote.java
+++ Notes/app/src/main/java/com/pucha/pavan/notes/EditYourNote.java
@@ -10,12 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static android.R.attr.editable;
 import static android.os.Build.VERSION_CODES.M;
 import static com.pucha.pavan.notes.MainActivity.notes;
 import static com.pucha.pavan.notes.MainActivity.set;
@@ -52,15 +56,42 @@ public class EditYourNote extends AppCompatActivity implements TextWatcher {
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.pucha.pavan.notes", Context.MODE_PRIVATE);
         if(MainActivity.set == null){
             MainActivity.set = new HashSet<String>();
-        }else
+        }else {
             MainActivity.set.clear();
-
+        }
         MainActivity.set.addAll(MainActivity.notes);
-        sharedPreferences.edit().putStringSet("notes", set).apply();
+        sharedPreferences.edit().remove("notes").apply();
+
+        sharedPreferences.edit().putStringSet("notes", MainActivity.set).apply();
+        for(String s: set){
+            Log.v(s," set element");
+        }
     }
 
     @Override
     public void afterTextChanged(Editable editable) {
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_your_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- Notes/app/src/main/java/com/pucha/pavan/notes/MainActivity.java
+++ Notes/app/src/main/java/com/pucha/pavan/notes/MainActivity.java
@@ -1,18 +1,26 @@
 package com.pucha.pavan.notes;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+
+import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity {
     static ArrayList<String> notes= new ArrayList<>();
@@ -25,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.listView);
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.pucha.pavan.notes", Context.MODE_PRIVATE);
         set = sharedPreferences.getStringSet("notes",null);
+        notes.clear();
         if(set!=null){
-            notes.clear();
+          //  notes.clear();
             notes.addAll(set);
         }
         else
@@ -37,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             set.addAll(notes);
             sharedPreferences.edit().putStringSet("notes",set).apply();
         }
-               
+
         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -52,6 +61,68 @@ public class MainActivity extends AppCompatActivity {
                 // As the item in list view is clicked ,  we automatically get the position,
             }
         });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int what){
+                                notes.remove(position);
+                                SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("com.pucha.pavan.notes", Context.MODE_PRIVATE);
+
+                                if(set == null){
+                                    set = new HashSet<String>();
+                                }else{
+                                    set.clear();}
+
+                                set.addAll(notes);
+                                sharedPreferences.edit().remove("notes").apply();
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No",null)
+                        .show();
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        int menuID = menuItem.getItemId();
+        if(menuID == R.id.Plus){
+            notes.add("");
+            SharedPreferences sharedPreferences = this.getSharedPreferences("com.pucha.pavan.notes", Context.MODE_PRIVATE);
+
+            if(set == null){
+                set = new HashSet<String>();
+            }else{
+                set.clear();}
+
+            set.addAll(notes);
+            arrayAdapter.notifyDataSetChanged();
+
+            sharedPreferences.edit().remove("notes").apply();
+            sharedPreferences.edit().putStringSet("notes", set).apply();
+            Intent i = new Intent(getApplicationContext(),EditYourNote.class);
+            i.putExtra("noteId", notes.size()-1);
+            startActivity(i);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
 
