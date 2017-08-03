73eb772b4d03c91be286ca5c4835454f81ae16ef
pavanpucha <pavanrajasekhar@gmail.com>
2017-08-03 18:26:16 -0400

    WebView with slight modifications

--- WebViewDemo/app/src/main/java/com/pucha/pavan/webviewdemo/MainActivity.java
+++ WebViewDemo/app/src/main/java/com/pucha/pavan/webviewdemo/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        //webView.loadUrl("http://www.google.com");
+        webView.loadData("<html><body><h1> OVERWATCH IN A NUTSHELL </h1><p> HEROES NEVER DIE </p></body></html>","text/html","UTF-8");
+
+
     }
 
     @Override
