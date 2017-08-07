d9ed22d7b6a54c64505691fab889ebae0c5290bc
pavanpucha <pavanrajasekhar@gmail.com>
2017-08-07 16:34:55 -0400

    Updated data retrival

--- HackerNewsReader/app/src/main/java/com/pucha/pavan/hackernewsreader/MainActivity.java
+++ HackerNewsReader/app/src/main/java/com/pucha/pavan/hackernewsreader/MainActivity.java
@@ -1,17 +1,32 @@
 package com.pucha.pavan.hackernewsreader;
 
 import android.os.AsyncTask;
+import android.support.v4.app.INotificationSideChannel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import static android.support.v7.widget.AppCompatDrawableManager.get;
+
 public class MainActivity extends AppCompatActivity {
+    Map<Integer,String> articleURLs = new HashMap<Integer, String>();
+    // Integer is article ID and String will store the url
+    Map<Integer,String> articleTitles = new HashMap<Integer, String>();
+    ArrayList<Integer>  articleIds = new ArrayList<Integer>();
+
+    // Creating a DB
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +34,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         DownloadTask newTask = new DownloadTask();
       try{
-          String result = newTask.execute("https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty").get();
-          Log.i("Result",result);
+          String result = newTask.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").get();
+          JSONArray jsonArray = new JSONArray(result);
+
+          for(int i =0; i< 20;i++){
+              String articleId = jsonArray.getString(i);
+              DownloadTask getArticle= new DownloadTask();
+
+              String articleInfo = getArticle.execute("https://hacker-news.firebaseio.com/v0/item/"+ jsonArray.getString(i)+".json?print=pretty").get();
+              JSONObject jsonObject = new JSONObject(articleInfo);
+              Log.i("jsonObject",jsonObject.toString());
+              String articleTitle = jsonObject.getString("title");
+              String articleUrl = jsonObject.getString("url");
+              articleIds.add(Integer.valueOf(articleId));
+              articleTitles.put(Integer.valueOf(articleId),articleTitle);
+              articleURLs.put(Integer.valueOf(articleId),articleUrl);
+
+          }
+          Log.i("Article ID's", articleIds.toString());
+          Log.i("Article Titles ", articleTitles.toString());
+          Log.i("Article urL", articleURLs.toString());
       }
-      catch (ExecutionException e){
+      catch (Exception e){
           e.printStackTrace();
       }
-      catch (InterruptedException e){
-          e.printStackTrace();
-      }
+
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String>{
